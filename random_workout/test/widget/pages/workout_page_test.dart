2165a9545af2e7315a1abf3238f26788a6aa26e2
Brylie Christopher Oxley <brylie@protonmail.com>
2024-07-28 11:34:39 +0300

    Fix/reorganize tests

--- random_workout/test/widget/pages/workout_page_test.dart
+++ random_workout/test/widget/pages/workout_page_test.dart
@@ -6,7 +6,7 @@ import 'package:random_workout/models/exercise.dart';
 import 'package:random_workout/pages/workout_page.dart';
 import 'package:random_workout/providers/app_state.dart';
 import 'package:random_workout/widgets/exercise/add_exercise_options_dialog.dart';
-import '../mocks/mock_app_state.mocks.dart';
+import '../../mocks/mock_app_state.dart';
 
 void main() {
   late MockAppState mockAppState;
@@ -44,9 +44,6 @@ void main() {
     expect(find.text('Add Exercise'), findsOneWidget);
     expect(find.text('Choose from pre-defined exercises'), findsOneWidget);
     expect(find.text('Create a custom exercise'), findsOneWidget);
-
-    verifyNever(mockAppState.generateWorkout());
-    verifyNever(mockAppState.addExercise(any));
   });
 
   testWidgets('Generate new workout button calls generateWorkout',
@@ -67,7 +64,6 @@ void main() {
     await tester.pumpAndSettle();
 
     verify(mockAppState.generateWorkout()).called(1);
-    verifyNever(mockAppState.addExercise(any));
   });
 
   testWidgets('WorkoutPage displays current workout',
@@ -123,46 +119,5 @@ void main() {
     );
 
     expect(find.text('Strength Workout'), findsOneWidget);
-  });
-
-  testWidgets('Workout diversity score is calculated correctly',
-      (WidgetTester tester) async {
-    final testExercises = [
-      Exercise(
-        name: 'Push-ups',
-        description: 'Do push-ups',
-        instructions: 'Do push-ups',
-        category: ExerciseCategory.strength,
-        muscleTargets: [MuscleTarget.chest, MuscleTarget.triceps],
-        jointTargets: [JointTarget.elbow],
-        focusAreas: [FocusArea.upperBody],
-      ),
-      Exercise(
-        name: 'Squats',
-        description: 'Do squats',
-        instructions: 'Do squats',
-        category: ExerciseCategory.strength,
-        muscleTargets: [MuscleTarget.quadriceps],
-        jointTargets: [JointTarget.knee],
-        focusAreas: [FocusArea.lowerBody],
-      ),
-    ];
-
-    when(mockAppState.currentWorkout).thenReturn(testExercises);
-    when(mockAppState.selectedCategory).thenReturn(ExerciseCategory.strength);
-    when(mockAppState.workoutDiversityScore).thenReturn(4);
-
-    await tester.pumpWidget(
-      MaterialApp(
-        home: ChangeNotifierProvider<AppState>.value(
-          value: mockAppState,
-          child: const WorkoutPage(),
-        ),
-      ),
-    );
-
-    expect(find.text('Push-ups'), findsOneWidget);
-    expect(find.text('Squats'), findsOneWidget);
-    expect(find.text('Diversity Score: 4'), findsOneWidget);
   });
 }
